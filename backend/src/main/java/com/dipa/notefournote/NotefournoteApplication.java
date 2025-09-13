045fb3acaafae8cbd62f16446da205d90a662c26
Vincenzo Caputo <vincenzo.caputo@hotmail.com>
2025-09-13 16:26:50 +0200

    feat: Bootstrap backend with initial infrastructure and config (Docker Compose and Actuator)

---
+++ backend/src/main/java/com/dipa/notefournote/NotefournoteApplication.java
@@ -0,0 +1,13 @@
+package com.dipa.notefournote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotefournoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotefournoteApplication.class, args);
+	}
+
+}
